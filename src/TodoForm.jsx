af70032552f7a2f137033852c8fa549796970de8
Melissa Perez <perezmelissa053@gmail.com>
2025-04-12 18:17:28 -0400

    feat: convert TodoForm to controlled component

--- src/TodoForm.jsx
+++ src/TodoForm.jsx
@@ -1,21 +1,29 @@
-import { useRef } from 'react';
+import { useState, useRef } from 'react';
 
 function TodoForm({ onAddTodo }) {
+  const [workingTodo, setWorkingTodo] = useState('');
   const todoTitleInput = useRef('');
   const id = Date.now();
 
   const handleAddTodo = (event) => {
     event.preventDefault();
-    const title = event.target.title.value;
-    onAddTodo({ title, id });
-    event.target.title.value = '';
+    onAddTodo({ title: workingTodo, id });
     todoTitleInput.current.focus();
+    setWorkingTodo('');
   };
   return (
     <>
       <form onSubmit={handleAddTodo}>
         <label htmlFor="todoTitle">Todo</label>
-        <input type="text" id="todoTitle" name="title" ref={todoTitleInput} />
+        <input
+          type="text"
+          id="todoTitle"
+          value={workingTodo}
+          onChange={(event) => {
+            setWorkingTodo(event.target.value);
+          }}
+          ref={todoTitleInput}
+        />
         <button>Add Todo</button>
       </form>
     </>
