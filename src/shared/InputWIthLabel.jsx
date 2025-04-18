fafa6299bf1fe4d5db006e8a1f20b4f6d8a18ad0
Melissa Perez <perezmelissa053@gmail.com>
2025-04-18 19:22:39 -0400

    feat: add InputWithLabel comp

---
+++ src/shared/InputWIthLabel.jsx
@@ -0,0 +1,16 @@
+function InputWithLabel({ elementId, label, onChange, value, ref }) {
+  return (
+    <>
+      <label htmlFor={elementId}>{label}</label>
+      <input
+        type="text"
+        id={elementId}
+        value={value}
+        onChange={onChange}
+        ref={ref}
+      />
+    </>
+  );
+}
+
+export default InputWithLabel;
