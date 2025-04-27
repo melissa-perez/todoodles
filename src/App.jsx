ef3b6bd94ad906a09aef034018182956e294e944
Melissa Perez <perezmelissa053@gmail.com>
2025-04-27 00:52:27 -0400

    feat: connects to airtable api to display message, a loading <p> and TODO: finish dismiss button

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,46 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import './App.css';
 import TodoList from './features/TodoList/TodoList';
 import TodoForm from './features/TodoForm';
 
 function App() {
   const [todoList, setTodoList] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const [errorMessage, setErrorMessage] = useState('');
+
+  useEffect(() => {
+    const fetchTodos = async () => {
+      const url = `https://api.airtable.com/v0/${import.meta.env.VITE_BASE_ID}/${import.meta.env.VITE_TABLE_NAME}`;
+      const token = `Bearer ${import.meta.env.VITE_PAT}`;
+      setIsLoading(true);
+      const options = {
+        method: 'GET',
+        headers: { Authorization: token },
+      };
+      try {
+        const resp = await fetch(url, options);
+        if (!resp.ok) {
+          throw new Error(resp.message);
+        }
+        const response = await resp.json();
+        console.log(response);
+        const fetchedTodos = response.records.map((record) => {
+          const todo = {
+            id: record.id,
+            ...record.fields,
+          };
+          if (!todo.isCompleted) todo.isCompleted = false;
+          return todo;
+        });
+        setTodoList([...fetchedTodos]);
+      } catch (error) {
+        setErrorMessage(error.message);
+      } finally {
+        setIsLoading(false);
+      }
+    };
+    fetchTodos();
+  }, []);
 
   const addTodo = (newTodo) => {
     newTodo.isCompleted = false;
@@ -35,7 +71,17 @@ function App() {
         todos={todoList}
         onCompleteTodo={completeTodo}
         onUpdateTodo={updateTodo}
+        isLoading={isLoading}
       />
+      {errorMessage ? (
+        <>
+          <hr />
+          <p>{errorMessage}</p>
+          <button>Dismiss Error Message</button>
+        </>
+      ) : (
+        <></>
+      )}
     </div>
   );
 }
