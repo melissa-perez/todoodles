e49493fec4defd2ae4d1b3f3e763cca98957a02d
Melissa Perez <perezmelissa053@gmail.com>
2025-04-27 23:21:50 -0400

    feat: add ability to add new todos to airtable, list currently out of order

--- src/App.jsx
+++ src/App.jsx
@@ -7,24 +7,24 @@ function App() {
   const [todoList, setTodoList] = useState([]);
   const [isLoading, setIsLoading] = useState(false);
   const [errorMessage, setErrorMessage] = useState('');
+  const [isSaving, setIsSaving] = useState(false);
 
   useEffect(() => {
+    const url = `https://api.airtable.com/v0/${import.meta.env.VITE_BASE_ID}/${import.meta.env.VITE_TABLE_NAME}`;
+    const token = `Bearer ${import.meta.env.VITE_PAT}`;
     const fetchTodos = async () => {
-      const url = `https://api.airtable.com/v0/${import.meta.env.VITE_BASE_ID}/${import.meta.env.VITE_TABLE_NAME}`;
-      const token = `Bearer ${import.meta.env.VITE_PAT}`;
       setIsLoading(true);
       const options = {
         method: 'GET',
-        headers: { Authorization: token },
+        headers: { Authorization: token, 'Content-Type': 'application/json' },
       };
       try {
         const resp = await fetch(url, options);
         if (!resp.ok) {
           throw new Error(resp.message);
         }
-        const response = await resp.json();
-        console.log(response);
-        const fetchedTodos = response.records.map((record) => {
+        const data = await resp.json();
+        const fetchedTodos = data.records.map((record) => {
           const todo = {
             id: record.id,
             ...record.fields,
@@ -42,9 +42,52 @@ function App() {
     fetchTodos();
   }, []);
 
-  const addTodo = (newTodo) => {
-    newTodo.isCompleted = false;
-    setTodoList([...todoList, newTodo]);
+  const addTodo = async (newTodo) => {
+    const url = `https://api.airtable.com/v0/${import.meta.env.VITE_BASE_ID}/${import.meta.env.VITE_TABLE_NAME}`;
+    const token = `Bearer ${import.meta.env.VITE_PAT}`;
+    const payload = {
+      records: [
+        {
+          fields: {
+            title: newTodo.title,
+            isCompleted: newTodo.isCompleted,
+          },
+        },
+      ],
+    };
+    const options = {
+      method: 'POST',
+      headers: {
+        Authorization: token,
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(payload),
+    };
+    console.log(newTodo);
+    try {
+      setIsSaving(true);
+      const resp = await fetch(url, options);
+      console.log(resp);
+
+      if (!resp.ok) {
+        throw new Error(resp.message);
+      }
+      const { records } = await resp.json();
+
+      const savedTodo = {
+        id: records[0].id,
+        ...records[0].fields,
+      };
+
+      if (!records[0].fields.isCompleted) savedTodo.isCompleted = false;
+
+      setTodoList([...todoList, savedTodo]);
+    } catch (error) {
+      console.error(error);
+      setErrorMessage(error.message);
+    } finally {
+      setIsSaving(false);
+    }
   };
 
   const completeTodo = (id) => {
