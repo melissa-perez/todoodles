5d2c9e22550609fef9b7a1baed905a37b3035aac
Melissa Perez <perezmelissa053@gmail.com>
2025-04-28 22:25:26 -0400

    readd code lost in merge

--- src/App.jsx
+++ src/App.jsx
@@ -42,9 +42,66 @@ function App() {
     fetchTodos();
   }, []);
 
-  const addTodo = (newTodo) => {
-    newTodo.isCompleted = false;
-    setTodoList([...todoList, newTodo]);
+  const addTodo = async (newTodo) => {
+    const url = `https://api.airtable.com/v0/${import.meta.env.VITE_BASE_ID}/${import.meta.env.VITE_TABLE_NAME}`;
+
+    const token = `Bearer ${import.meta.env.VITE_PAT}`;
+
+    const payload = {
+      records: [
+        {
+          fields: {
+            title: newTodo.title,
+
+            isCompleted: newTodo.isCompleted,
+          },
+        },
+      ],
+    };
+
+    const options = {
+      method: 'POST',
+
+      headers: {
+        Authorization: token,
+
+        'Content-Type': 'application/json',
+      },
+
+      body: JSON.stringify(payload),
+    };
+
+    console.log(newTodo);
+
+    try {
+      setIsSaving(true);
+
+      const resp = await fetch(url, options);
+
+      console.log(resp);
+
+      if (!resp.ok) {
+        throw new Error(resp.message);
+      }
+
+      const { records } = await resp.json();
+
+      const savedTodo = {
+        id: records[0].id,
+
+        ...records[0].fields,
+      };
+
+      if (!records[0].fields.isCompleted) savedTodo.isCompleted = false;
+
+      setTodoList([...todoList, savedTodo]);
+    } catch (error) {
+      console.error(error);
+
+      setErrorMessage(error.message);
+    } finally {
+      setIsSaving(false);
+    }
   };
 
   const completeTodo = (id) => {
@@ -89,7 +146,7 @@ function App() {
       const { records } = await resp.json();
       const updatedTodo = {
         id: records[0]['id'],
-        ...records.fields,
+        ...records[0].fields,
       };
 
       if (!records[0].fields.isCompleted) updatedTodo.isCompleted = false;
