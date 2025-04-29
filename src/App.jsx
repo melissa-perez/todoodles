0895f420902ea9b9ae2c4e8343fccab64f17b0dd
Melissa Perez <perezmelissa053@gmail.com>
2025-04-28 22:27:29 -0400

    refactor: remove extra line blanks

--- src/App.jsx
+++ src/App.jsx
@@ -44,9 +44,7 @@ function App() {
 
   const addTodo = async (newTodo) => {
     const url = `https://api.airtable.com/v0/${import.meta.env.VITE_BASE_ID}/${import.meta.env.VITE_TABLE_NAME}`;
-
     const token = `Bearer ${import.meta.env.VITE_PAT}`;
-
     const payload = {
       records: [
         {
@@ -58,46 +56,30 @@ function App() {
         },
       ],
     };
-
     const options = {
       method: 'POST',
-
       headers: {
         Authorization: token,
-
         'Content-Type': 'application/json',
       },
-
       body: JSON.stringify(payload),
     };
 
-    console.log(newTodo);
-
     try {
       setIsSaving(true);
-
       const resp = await fetch(url, options);
-
-      console.log(resp);
-
       if (!resp.ok) {
         throw new Error(resp.message);
       }
-
       const { records } = await resp.json();
-
       const savedTodo = {
         id: records[0].id,
-
         ...records[0].fields,
       };
-
       if (!records[0].fields.isCompleted) savedTodo.isCompleted = false;
-
       setTodoList([...todoList, savedTodo]);
     } catch (error) {
       console.error(error);
-
       setErrorMessage(error.message);
     } finally {
       setIsSaving(false);
